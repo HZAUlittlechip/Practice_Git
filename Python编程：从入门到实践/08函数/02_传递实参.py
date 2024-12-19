0ae730b9d977db0832d28dddd5784542902f785d
HZAUlittlechip <157097025+HZAUlittlechip@users.noreply.github.com>
2024-12-19 13:30:17 +0800

    添加

--- Python编程：从入门到实践/08函数/02_传递实参.py
+++ Python编程：从入门到实践/08函数/02_传递实参.py
@@ -20,3 +20,26 @@ describe_pet('cat', 'tom')
 # 02. 位置实参的顺序很重要
 
 # 8.2.2 关键字实参
+
+def describe_pet(animal_type, pet_name):
+	"""显⽰宠物的信息"""
+	print(f"\nI have a {animal_type}.")
+	print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet(animal_type= 'hamster',pet_name= 'harry')
+
+# 设置默认值
+
+# 默认值的参数要放在无默认值形参后面
+# python 按顺序将 第一个实参 连接到 第一个行参
+def describe_pet(pet_name, animal_type = 'dog'):
+	"""显⽰宠物的信息"""
+	print(f"\nI have a {animal_type}.")
+	print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet('harry')
+
+# 默认值 可以在调用时修改
+describe_pet(pet_name='harry', animal_type='hamster')
+
+# 8.2.4 等效的函数调⽤
--- Python编程：从入门到实践/08函数/HW07.py
+++ Python编程：从入门到实践/08函数/HW07.py
@@ -19,3 +19,51 @@ def  favorite_book(title):
 	print(f'One of my favorite books is {title}')
 
 favorite_book('JOKER')
+
+# 练习 8.3：T 恤 编写⼀个名为 make_shirt() 的函数，它接受⼀个
+# 尺码以及要印到 T 恤上的字样。这个函数应该打印⼀个句⼦，简要地
+# 说明 T 恤的尺码和字样。
+# 先使⽤位置实参调⽤这个函数来制作⼀件 T 恤，再使⽤关键字实参来
+# 调⽤这个函数
+
+
+def make_shirt(size,word):
+	print(f'这件衣服的尺码是{size}，上面的字样是{word}')
+
+# 位置参数
+make_shirt(36,'Just do it!')
+
+# 关键词参数
+make_shirt(word='Just do it!',size= 36)
+
+# 练习 8.4：⼤号 T 恤 修改 make_shirt() 函数，使其在默认情况下
+# 制作⼀件印有
+# “I love Python”字样的⼤号 T 恤。调⽤这个函数分别制作⼀件印有默认
+# 字样的⼤号 T 恤，⼀件印有默认字样的中号 T 恤，以及⼀件印有其他
+# 字样的 T 恤（尺码⽆关紧要）。
+
+def make_shirt(size,word = 'I love python'):
+	print(f'这件衣服的尺码是{size}，上面的字样是{word}')
+
+# 大号 T
+make_shirt('big')
+
+# 中号 I
+make_shirt('medium')
+
+# 别的字样
+make_shirt('medium',word= '嘿嘿')
+
+# 练习 8.5：城市 编写⼀个名为 describe_city() 的函数，它接受
+# ⼀座城市的名字以及该城市所属的国家。这个函数应该打印⼀个像下
+# ⾯这样简单的句⼦。
+# Reykjavik is in Iceland.
+# 给⽤于存储国家的形参指定默认值。为三座不同的城市调⽤这个函
+# 数，其中⾄少有⼀座城市不属于默认的国家
+
+def describe_city(city_name, counrty = 'China'):
+	print(f'{city_name} is in {counrty}')
+
+describe_city('wuhan')
+describe_city('dalian')
+describe_city('xiaoliucheng',counrty= 'xiaoliuguo')
